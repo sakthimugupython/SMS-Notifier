db31f26f1de41a07a152c167730fabf4befa1ddf
sakthimugureact <mugu.8533@gmail.com>
2025-06-20 21:58:26 +0530

    initital

--- Code.py
+++ Code.py
@@ -1,21 +1,21 @@
-from twilio.rest import Client
+# from twilio.rest import Client
 
-# account_sid = 'AC9d63142d4ac6d623b914bddac0f91548'
-# auth_token = 'f0c5f9b4a92254fb6e6d1a94ebac2bc2'
-# twilio_number = '+14845378950'
+# # account_sid = 'AC9d63142d4ac6d623b914bddac0f91548'
+# # auth_token = 'f0c5f9b4a92254fb6e6d1a94ebac2bc2'
+# # twilio_number = '+14845378950'
 
-def send_sms(to_number, message_body):
-    try:
-        client = Client(account_sid, auth_token)
-        message = client.messages.create(
-            body=message_body,
-            from_=twilio_number,
-            to=to_number
-        )
-        print(f"SMS sent! Message SID: {message.sid}")
-    except Exception as e:
-        print(f"Failed to send SMS: {e}")
+# def send_sms(to_number, message_body):
+#     try:
+#         client = Client(account_sid, auth_token)
+#         message = client.messages.create(
+#             body=message_body,
+#             from_=twilio_number,
+#             to=to_number
+#         )
+#         print(f"SMS sent! Message SID: {message.sid}")
+#     except Exception as e:
+#         print(f"Failed to send SMS: {e}")
 
-to = input("Enter recipient number (e.g., +91xxxxxxxxxx): ").strip()
-msg = input("Enter your message: ").strip()
-send_sms(to, msg)
+# to = input("Enter recipient number (e.g., +91xxxxxxxxxx): ").strip()
+# msg = input("Enter your message: ").strip()
+# send_sms(to, msg)
